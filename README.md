464e0068469256640812378ccd3c024c0cce1532
Hood <hood@mit.edu>
2020-03-18 07:28:53 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,28 @@
+This project is a descendent of the prototype [js_spectralsequences](https://github.com/hoodmane/js_spectralsequences).
+
+# Compilation
+
+The directory src/ contains the source code for the spectral-sequences module.
+This is compiled into target/release/spectralsequences_basic_webclient.js, which is what you should include into your
+webpage.
+
+To install dependencies for compilation, run 
+```
+ $ npm install
+```
+This only has to be done once. Afterwards, to compile the source code into
+target/release/spectralsequences_basic_webclient.js, run
+```
+ $ npm run build
+```
+# Running
+
+Due to CORS restrictions, opening an file directly in a browser is unlikely to
+succeed. Instead, you should run a web server that serves the directory and
+then access localhost. To do so, navigate into the directory containing the
+source code, and run one of the following two commans:
+```
+ $ python3 -m http.server 8080
+ $ npx http-server
+```
+Afterwards, direct your browser to http://localhost:8080/
---
+++ src/sseq/utils.js
@@ -0,0 +1,57 @@
+function KeyError(message) {
+    this.message = message;
+    this.name = 'KeyError';
+    console.error(message);
+}
+
+exports.assign_fields = function assign_fields(obj, kwargs, fields) {
+    for(let field of fields) {
+        switch(field["type"]) {
+            case "mandatory":
+                exports.assign_kwarg_mandatory(obj, kwargs, field["field"]);
+                break;
+            case "optional":
+                exports.assign_kwarg_optional(obj, kwargs, field["field"]);
+                break;
+            case "default":
+                exports.assign_kwarg_default(obj, kwargs, field["field"], field["default"]);
+                break;
+            default:
+                throw new KeyError(`Unknown field type ${field["type"]}`);
+        }
+    }
+}
+
+exports.assign_kwarg_mandatory = function assign_kwarg_mandatory(obj, kwargs, field) {
+    if(kwargs === undefined){
+        throw new KeyError("kwargs is undefined!");
+    }
+    if(kwargs[field] === undefined) {
+        throw new KeyError(`Missing mandatory argument ${field}`);
+    } else {
+        obj[field] = kwargs[field];
+    }
+}
+
+exports.assign_kwarg_optional = function assign_kwarg_optional(obj, kwargs, field) {
+    if(kwargs === undefined){
+        throw new KeyError("kwargs is undefined!");
+    }
+    if(kwargs[field] === undefined) {
+
+    } else {
+        obj[field] = kwargs[field]
+    }
+}
+
+
+exports.assign_kwarg_default = function assign_kwarg_default(obj, kwargs, field, default_value) {
+    if(kwargs === undefined) {
+        throw new KeyError("kwargs is undefined!");
+    }
+    if(kwargs[field] === undefined) {
+        obj[field] = default_value
+    } else {
+        obj[field] = kwargs[field]
+    }
+}
